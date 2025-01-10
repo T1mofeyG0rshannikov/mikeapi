17a3b5d3528e0ae27042e7c38ec441e247b67c10
TimofeyNefr <tgorshannikov@mail.ru>
2025-01-10 22:29:10 +0800

    traders list

--- src/admin/views.py
+++ src/admin/views.py
@@ -43,6 +43,14 @@ class APIUrlsAdmin(ModelView, model=APIURLSOrm):
     name_plural = "url адреса"
 
 
+from fastapi.requests import Request
+from markupsafe import Markup
+from sqladmin.pagination import Pagination
+from sqlalchemy import func
+from sqlalchemy.orm import selectinload
+from sqlalchemy.sql.expression import select
+
+
 class TraderAdmin(ModelView, model=TraderOrm):
     column_list = [
         TraderOrm.id,
@@ -57,7 +65,50 @@ class TraderAdmin(ModelView, model=TraderOrm):
         TraderOrm.badges,
     ]
 
-    list_template = "sqladmin/list.html"
+    list_template = "sqladmin/list-traders.html"
 
     name = "Трейдер"
     name_plural = "Трейдеры"
+
+    column_sortable_list = ["status"]
+
+    column_formatters = {
+        TraderOrm.username: lambda trader, _: Markup(
+            f"""<a href="https://www.tbank.ru/invest/social/profile/{trader.username}/" target="_blank">{trader.username}</a>"""
+        )
+    }
+
+    async def list(self, request: Request) -> Pagination:
+        page = self.validate_page_number(request.query_params.get("page"), 1)
+        page_size = self.validate_page_number(request.query_params.get("pageSize"), 0)
+        page_size = min(page_size or self.page_size, max(self.page_size_options))
+        search = request.query_params.get("search", None)
+        status = request.query_params.get("status")
+
+        stmt = self.list_query(request)
+
+        if status:
+            stmt = stmt.filter(TraderOrm.status == status)
+
+        for relation in self._list_relations:
+            stmt = stmt.options(selectinload(relation))
+
+        stmt = self.sort_query(stmt, request)
+
+        if search:
+            stmt = self.search_query(stmt=stmt, term=search)
+            count = await self.count(request, select(func.count()).select_from(stmt))
+        else:
+            count = await self.count(request, select(func.count()).select_from(stmt))
+
+        stmt = stmt.limit(page_size).offset((page - 1) * page_size)
+        rows = await self._run_query(stmt)
+
+        pagination = Pagination(
+            rows=rows,
+            page=page,
+            page_size=page_size,
+            count=count,
+        )
+
+        return pagination
